de0d6a5fb0a79ca77cc67cb96e1b2569d55d242e
Behruz <113906364+Behruz746@users.noreply.github.com>
2023-09-06 20:15:24 +0500

    Update Faq.jsx

--- src/pages/help/Faq.jsx
+++ src/pages/help/Faq.jsx
@@ -7,7 +7,7 @@ function Faq() {
       <ul>
         <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, dolores recusandae. Natus, ipsam tempore expedita perspiciatis aperiam, ullam, odit suscipit eius labore obcaecati sapiente repellendus? Non vero id animi eos quia tempore perferendis voluptatem vitae similique pariatur obcaecati, sunt necessitatibus nam iure maiores saepe, magnam ipsum. Totam in cupiditate cumque.</li>
         <li>Cumque omnis nam eligendi, officia deleniti eos vero debitis nulla. Molestias asperiores vitae numquam ducimus, voluptas amet, possimus non laboriosam ullam fuga voluptate in architecto qui est obcaecati hic! Ad asperiores corrupti earum maiores expedita non aut mollitia! Perferendis quis, in modi quam aut sunt error? Iure aliquam magni at.</li>
-        <li>Voluptatum eos officiis debitis, voluptas nisi dolore rerum quae pariatur at nulla quidem cum iusto, dolorum eaque eveniet, quis in culpa sequi doloremque! Cum quisquam quaerat fugiat hic voluptatum doloremque minus? Recusandae rem aspernatur quo provident doloremque nobis, porro quis? Dolorem nesciunt deleniti corrupti officia atque quidem dolorum esse nihil!</li>
+        <li>Voluptatum eos officiis hi my name is Behruz my fullname is Madaminov Behruz i am interested in typing code foy web apps i am very very like build web sites  debitis, voluptas nisi dolore rerum quae pariatur at nulla quidem cum iusto, dolorum eaque eveniet, quis in culpa sequi doloremque! Cum quisquam quaerat fugiat hic voluptatum doloremque minus? Recusandae rem aspernatur quo provident doloremque nobis, porro quis? Dolorem nesciunt deleniti corrupti officia atque quidem dolorum esse nihil!</li>
       </ul>
     </div>
   )
