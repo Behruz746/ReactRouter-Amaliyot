d8fdcf31f036e083eef4143492c514aa23ada9c9
Behruzbek <madaminovbehruz746@gmail.com>
2023-09-12 17:33:13 +0500

    update commit

---
+++ src/components/BreadCrumbs.jsx
@@ -0,0 +1,11 @@
+import { NavLink, useLocation } from 'react-router-dom';
+
+function BreadCrumbs() {
+  return (
+    <div>
+        sasa
+    </div>
+  )
+}
+
+export default BreadCrumbs;
--- src/layout/RootLyout.jsx
+++ src/layout/RootLyout.jsx
@@ -1,36 +1,44 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 import Logo from "../assets/react-router.svg";
+import BreadCrumbs from "../components/BreadCrumbs";
 import "../components/sass/Header.scss";
 
 function RootLyout() {
   return (
     <>
       <header className="App__header">
-        <div className="Header__logo">
-          <NavLink to="/">
-            <img src={Logo} alt="Logo" className="Logo" />
-          </NavLink>
+        <div className="Header__container">
+          <div className="Header__logo">
+            <NavLink to="/">
+              <img src={Logo} alt="Logo" className="Logo" />
+            </NavLink>
+          </div>
+          <nav className="Header__nav">
+            <ul className="nav__list">
+              <li className="list__items">
+                <NavLink to="/" className="item__link">
+                  Home
+                </NavLink>
+              </li>
+              <li className="list__items">
+                <NavLink to="/about" className="item__link">
+                  About
+                </NavLink>
+              </li>
+              <li className="list__items">
+                <NavLink to="/contact" className="item__link">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="Header__nav">
-          <ul className="nav__list">
-            <li className="list__items">
-              <NavLink to="/" className="item__link">
-                Home
-              </NavLink>
-            </li>
-            <li className="list__items">
-              <NavLink to="/about" className="item__link">
-                About
-              </NavLink>
-            </li>
-            <li className="list__items">
-              <NavLink to="/contact" className="item__link">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+
+        <div className="Header__map">
+          <BreadCrumbs />
+        </div>
+        
       </header>
 
       <main className="App__main">
